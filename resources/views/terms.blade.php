c27aa4d480721950a94f979099b29f11352451b0
joeyfasanelli <joeyfas1704@gmail.com>
2023-08-18 15:42:01 -0400

    add layouts

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div>
+    <h1>Terms</h1><br>
+    Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out printgraphic or web designs. The passage is attributed to an unknown typesetter in the 15tcentury who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorufor use in a type specimen book. It usually begins with:
+    <br></br>
+    “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunut labore et dolore magna aliqua.”
+    <br></br>
+    The purpose of lorem ipsum is to create a natural looking block of text (sentenceparagraph, page, etc.) that doesn't distract from the layout. A practice not withoucontroversy, laying out pages with meaningless filler text can be very useful when thfocus is meant to be on design, not content.
+    <br></br>
+    The passage experienced a surge in popularity during the 1960s when Letraset used it otheir dry-transfer sheets, and again during the 90s as desktop publishers bundled the texwith their software. Today it's seen all around the web; on templates, websites, and stocdesigns. Use our generator to get your own, or read on for the authoritative history olorem ipsum. 
+</div>
+
+    
+@endsection
+
+
+    
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ use App\Http\Controllers\ProfileController;
 
 
 Route::get('/', [ DashboardController::class , 'index' ] );
-Route::get('/profile', [ ProfileController::class , 'index' ] );
+
+Route::get('/terms', function(){
+    return view('terms');
+});
 
